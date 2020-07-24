7a0becaf394eaea98b90a4e4d47254d7f375bc19
Anaminus <anaminus@protonmail.com>
2020-07-24 10:50:58 -0500

    Add value interfaces.

---
+++ value.go
@@ -0,0 +1,38 @@
+package types
+
+// Value is a value that has a type.
+type Value interface {
+	Type() string
+}
+
+// Stringer is any Value that has a string representation.
+type Stringer interface {
+	Value
+	String() string
+}
+
+// PropValue is a Value that can be copied.
+type PropValue interface {
+	Value
+	Copy() PropValue
+}
+
+// Stringlike is any Value that is string-like. Note that this is distinct from
+// a string-representation of the value. Rather, Stringlike indicates that the
+// value has string-like properties.
+type Stringlike interface {
+	Value
+	Stringlike() string
+}
+
+// Numberlike is any value that can be converted to a floating-point number.
+type Numberlike interface {
+	Value
+	Numberlike() float64
+}
+
+// Intlike is any value that can be converted to an integer.
+type Intlike interface {
+	Value
+	Intlike() int64
+}
