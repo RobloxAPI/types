5f5273da2e342d54544544bfd75cdaad6b9fb6bc
Anaminus <anaminus@protonmail.com>
2021-01-19 09:11:29 -0600

    Implement NewColor3FromHSV and Color.ToHSV.

--- Color3.go
+++ Color3.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"math"
 	"strconv"
 )
 
@@ -16,8 +17,15 @@ func NewColor3FromRGB(r, g, b int) Color3 {
 	}
 }
 
-func NewColor3FromHSV(h, s, v float64) Color3 {
-	panic("not implemented")
+func NewColor3FromHSV(h, s, v float64) (c Color3) {
+	var k float64
+	k = math.Mod(5+h*6, 6)
+	c.R = float32(v - v*s*math.Max(0, math.Min(math.Min(k, 4-k), 1)))
+	k = math.Mod(3+h*6, 6)
+	c.G = float32(v - v*s*math.Max(0, math.Min(math.Min(k, 4-k), 1)))
+	k = math.Mod(1+h*6, 6)
+	c.B = float32(v - v*s*math.Max(0, math.Min(math.Min(k, 4-k), 1)))
+	return c
 }
 
 func (c Color3) Lerp(goal Color3, alpha float64) Color3 {
@@ -31,7 +39,29 @@ func (c Color3) Lerp(goal Color3, alpha float64) Color3 {
 }
 
 func (c Color3) ToHSV() (h, s, v float64) {
-	panic("not implemented")
+	var r, g, b = float64(c.R), float64(c.G), float64(c.B)
+	min := math.Min(math.Min(r, g), b)
+	max := math.Max(math.Max(r, g), b)
+	delta := max - min
+	v = max
+	if max != 0 {
+		s = delta / max
+	} else {
+		s = 0
+		h = -1
+	}
+	switch max {
+	case r:
+		h = ((g - b) / delta) / 6
+	case g:
+		h = (2 + (b-r)/delta) / 6
+	case b:
+		h = (4 + (r-g)/delta) / 6
+	}
+	if h <= 0 {
+		h++
+	}
+	return h, s, v
 }
 
 // Type returns a string identifying the type.
