0ae0f1517088a56ebde241f35e6450168d115dc4
Anaminus <anaminus@protonmail.com>
2021-01-19 10:36:47 -0600

    Implement Region3.ExpandToGrid.

--- Region3.go
+++ Region3.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"math"
+)
+
 type Region3 struct {
 	Min, Max Vector3
 }
@@ -14,8 +18,31 @@ func (r Region3) Size() Vector3 {
 	return r.Max.Sub(r.Min)
 }
 
-func (r Region3) ExpandToGrid(res int) Region3 {
-	panic("not implemented")
+func sign(x float32) float32 {
+	switch {
+	case x < 0:
+		return -1
+	case x > 0:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func rafz3(v Vector3) Vector3 {
+	v.X = sign(v.X) * float32(math.Ceil(math.Abs(float64(v.X))))
+	v.Y = sign(v.Y) * float32(math.Ceil(math.Abs(float64(v.Y))))
+	v.Z = sign(v.Z) * float32(math.Ceil(math.Abs(float64(v.Z))))
+	return v
+}
+
+func (r Region3) ExpandToGrid(res float64) Region3 {
+	if res == 0 {
+		return r
+	}
+	r.Min = rafz3(r.Min.DivN(res)).MulN(res)
+	r.Max = rafz3(r.Max.DivN(res)).MulN(res)
+	return r
 }
 
 // Type returns a string identifying the type.
